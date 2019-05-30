1c036a58ebbfc8abb09efacd22ccaf1ac809e62d
JonathanHelvey <jonathanhelvey27@gmail.com>
2019-05-30 01:51:34 -0400

    graphQL

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,11 +10,19 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
         name: `images`,
         path: `${__dirname}/src/images`,
       },
     },
     `gatsby-transformer-sharp`,
+    `gatsby-transformer-remark`,
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
